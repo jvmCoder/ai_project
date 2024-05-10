74c495283c32f9ecf96c82db576789640a068587
jvmCoder <51904122+jvmCoder@users.noreply.github.com>
2024-05-10 16:20:11 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+#환경변수
+from dotenv import load_dotenv
+load_dotenv()
+
+#스트림릿
+import streamlit as st
+
+#ChatOpenAI
+#from langchain.chat_models import openai
+
+#OpenAI Call
+
+
+st.title("test")
+st.text_input("input!!")
+st.button("save")
+
+
+
